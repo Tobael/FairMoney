b6170999c24bf88819d5cbe961e027c04738f763
1Joghurt <no.mail@mail.de>
2024-12-01 16:34:06 +0100

    feat(Frontend): Added Header and Footer Components

---
+++ Frontend/src/components/Footer/footer.jsx
@@ -0,0 +1,9 @@
+import "./Footer.scss";
+
+const Footer = () => {
+    return (
+        <div id="footer">FairMoney</div>
+    );
+};
+
+export default Footer;
---
+++ Frontend/src/components/Header/header.jsx
@@ -0,0 +1,11 @@
+import "./header.scss";
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+
+export default function Header({onBackClick}) {
+    return (
+        <div id="header_container">
+            <ArrowBackIcon id="header_back_click" onClick={() => onBackClick()}/>
+            <div id="header_text">FairMoney</div>
+        </div>
+    );
+}
